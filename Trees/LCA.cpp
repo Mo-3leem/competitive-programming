70c11d3504d4fb30d35029518907d301117dff60
Mohammed Abdelaleem <166875636+Mo-3leem@users.noreply.github.com>
2026-05-03 20:34:36 +0300

    LCA updated

--- Trees/LCA.cpp
+++ Trees/LCA.cpp
@@ -1,70 +1,68 @@
-vector<vector<pair<int, int>>> adj; /// [v , w]
-vector<int> lvl;
-vector<vector<int>> dp, up;
-int LOG, initial;
+/*
+  How to use
+  LCA tree(n, adj);
+  tree.lca(u, v);
+*/
+struct LCA {
+  int n, LOG;
+  vector<vector<int> > up, adj;
+  vector<int> depth;
 
-int mrg(int u, int v) { return max(u, v); }/* TODO */
+  LCA(int n, vector<vector<int> > &g, int root = 0) {
+    this->n = n;
+    adj = g;
+    LOG = __lg(n) + 1;
 
-void dfs0(int u, int p = 0) {
-  for (int i = 1; i < LOG; i++) {
-    up[i][u] = up[i - 1][up[i - 1][u]];
-    dp[i][u] = mrg(dp[i - 1][u], dp[i - 1][up[i - 1][u]]);
-  }
-  for (auto [v, w]: adj[u]) {
-    if (v == p)continue;
-    up[0][v] = u;
-    dp[0][v] = w;
-    lvl[v] = lvl[u] + 1;
-    dfs0(v, u);
-  }
-}
+    up.assign(n, vector<int>(LOG));
+    depth.assign(n, 0);
 
-int LCA(int u, int v) {
-  if (lvl[u] < lvl[v]) {
-    swap(u, v);
+    dfs(root, root);
   }
-  int diff = lvl[u] - lvl[v];
-  for (int i = 0; diff; diff >>= 1, ++i) {
-    if (diff & 1) {
-      u = up[i][u];
+
+  void dfs(int u, int par) {
+    up[u][0] = par;
+
+    for (int j = 1; j < LOG; j++) {
+      up[u][j] = up[up[u][j - 1]][j - 1];
+    }
+
+    for (auto &v: adj[u]) {
+      if (v == par) continue;
+
+      depth[v] = depth[u] + 1;
+      dfs(v, u);
     }
   }
-  if (u == v)return u;
-  for (int i = LOG - 1; i >= 0; i--) {
-    if (up[i][u] != up[i][v]) {
-      u = up[i][u], v = up[i][v];
-    }
-  }
-  return up[0][u];
-}
 
-int query(int u, int v) {
-  if (lvl[u] < lvl[v]) {
-    swap(u, v);
-  }
-  int res = initial;
-  int diff = lvl[u] - lvl[v];
-  for (int i = 0; diff; ++i, diff >>= 1) {
-    if (diff & 1) {
-      res = mrg(res, dp[i][u]);
-      u = up[i][u];
+  int kthAncestor(int u, int k) {
+    for (int j = 0; j < LOG; j++) {
+      if (k & (1 << j)) {
+        u = up[u][j];
+      }
     }
+    return u;
   }
-  if (u == v)return res;
-  for (int i = LOG - 1; i >= 0; i--) {
-    if (up[i][u] != up[i][v]) {
-      res = mrg(res, mrg(dp[i][u], dp[i][v]));
-      u = up[i][u], v = up[i][v];
-    }
-  }
-  res = mrg(res, mrg(dp[0][u], dp[0][v]));
-  return res;
-}
 
-void compute(int root = 1) {
-  LOG = __lg(adj.size()) + 1;
-  lvl.assign(adj.size(), {});
-  dp.assign(LOG, vector<int>(adj.size()));
-  up.assign(LOG, vector<int>(adj.size()));
-  dfs0(root);
-}
+  int lca(int u, int v) {
+    if (depth[u] < depth[v])
+      swap(u, v);
+
+    u = kthAncestor(u, depth[u] - depth[v]);
+
+    if (u == v)
+      return u;
+
+    for (int j = LOG - 1; j >= 0; j--) {
+      if (up[u][j] != up[v][j]) {
+        u = up[u][j];
+        v = up[v][j];
+      }
+    }
+
+    return up[u][0];
+  };
+
+  int dist(int u, int v) {
+    return depth[u] + depth[v] - 2 * depth[lca(u, v)];
+  }
+};
