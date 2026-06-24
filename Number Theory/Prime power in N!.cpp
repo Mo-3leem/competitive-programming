505ec624abcac7395dec9d5c7dae1779771f638c
Mohammed Abdelaleem <166875636+Mo-3leem@users.noreply.github.com>
2026-06-24 22:59:17 +0300

    Refactor count_p_in_nfact to prevent overflow

--- Number Theory/Prime power in N!.cpp
+++ Number Theory/Prime power in N!.cpp
@@ -1,7 +1,11 @@
+// O(Log p(n))
+
 ll count_p_in_nfact(ll p, ll n) {
-  ll res = 0, q = p;
-  while (q <= n) {
-    res += n / q, q *= p;
-  }
-  return res;
+    ll res = 0;
+    for (ll q = p; q <= n; ) {
+        res += n / q;
+        if (q > n / p) break; // prevent overflow
+        q *= p;
+    }
+    return res;
 }
