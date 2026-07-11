77ddfefa742cef0bc7d39311aebe150c4a904151
Mohammed Abdelaleem <166875636+Mo-3leem@users.noreply.github.com>
2026-07-11 23:40:15 +0300

    Store bridges in a vector in Bridges.cpp
    Refactor Tarjan Bridge Algorithm implementation to store bridges in a vector.

--- Graph/Bridges.cpp
+++ Graph/Bridges.cpp
@@ -1,10 +1,14 @@
-///  O(N + M)
+// Tarjan Bridge Algorithm, A bridge is an edge whose removal increases the number of connected components.
+// Time: O(N + M)
+// Space: O(N + M)
+
 int n;
 vector<vector<int>> adj;
 
 vector<bool> visited;
 vector<int> tin, low;
 int timer;
+vector<pair<int, int>> bridges;
 
 void dfs(int v, int p = -1) {
   visited[v] = true;
@@ -17,18 +21,23 @@ void dfs(int v, int p = -1) {
       dfs(to, v);
       low[v] = min(low[v], low[to]);
       if (low[to] > tin[v])
-        IS_BRIDGE(v, to);
+        bridges.push_back({v, to});
     }
   }
 }
+int comp;
 
 void find_bridges() {
   timer = 0;
+  comp = 0;
+  bridges.clear();
   visited.assign(n, false);
   tin.assign(n, -1);
   low.assign(n, -1);
   for (int i = 0; i < n; ++i) {
-    if (!visited[i])
+    if (!visited[i]) {
+      comp++;
       dfs(i);
+    }
   }
 }
