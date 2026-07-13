49af7173db18d09ef76e851df45346818a26ac04
Mohammed Abdelaleem <166875636+Mo-3leem@users.noreply.github.com>
2026-07-13 19:43:22 +0300

    Refactor prime factorization using SPF method
    Refactor prime factorization implementation to use Smallest Prime Factor (SPF) method for efficiency. Update constants and improve comments for clarity.

--- Number Theory/Prime Factorization .cpp
+++ Number Theory/Prime Factorization .cpp
@@ -1,21 +1,13 @@
-// O(n*log(n))
-const int N = 1e5 + 10;
-bitset<N> bt;
-vector<pair<int, int>> adj[N];
+//=====================================================
+// Smallest Prime Factor (SPF)
+// Build: O(N log log N)
+// Space: O(N)
+// Query Prime Factorization: O(log n)
+//
+// Use when:
+// - Many factorization queries
+//=====================================================
 
-  bt.set();
-  bt[0] = bt[1] = 0;
-  for (int i = 2; i < N; i++) {
-    if (bt[i]) {
-      for (int j = i; j < N; j+=i) {
-        int cnt{}, tmp = j;
-        while (tmp % i == 0) tmp /= i, cnt++;
-        adj[j].emplace_back(i, cnt);
-      }
-    }
-  }
-
-// O(n*log(n))
 const int N = 1e6 + 5;
 int spf[N];
 
@@ -24,8 +16,27 @@ void buildSPF() {
 
     for (int i = 2; i * i < N; i++) {
         if (spf[i] != i) continue;
+
         for (int j = i * i; j < N; j += i)
             if (spf[j] == j)
                 spf[j] = i;
+}
+
+// return {{prime, exponent}, ...}
+vector<pair<int,int>> primeFactorization(int x) {
+    vector<pair<int,int>> fac;
+
+    while (x > 1) {
+        int p = spf[x];
+        int cnt = 0;
+
+        while (x % p == 0) {
+            x /= p;
+            cnt++;
+        }
+
+        fac.push_back({p, cnt});
     }
+
+    return fac;
 }
