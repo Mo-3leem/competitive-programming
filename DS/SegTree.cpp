3dc63b03b69f03cdfac24cac316128afd9cd4abe
Mohammed Abdelaleem <166875636+Mo-3leem@users.noreply.github.com>
2026-07-28 05:22:57 +0300

    Refactor SegTree implementation and improve clarity

--- DS/SegTree.cpp
+++ DS/SegTree.cpp
@@ -1,58 +1,77 @@
 struct SegTree {
-#define LF (x*2+1)
-#define RT (x*2+2)
-#define md ((lx+rx) >> 1)
-  vector<int> seg, lazy;
-  int ign;
-  int sz;
-
-  SegTree(int n) {
-    sz = n;
-    ign = 2e9; /// TODO::update this
-    seg.assign(4 * n, {});
-    lazy.assign(4 * n, {});
-  }
-
-  int merge(const int &lf, const int &rt) {
-    return min(lf, rt); /// update this
-  }
-
-  void propagate(int x, int lx, int rx) {
-    if (!lazy[x])return;
-    if (lx != rx) {
-      lazy[LF] += lazy[x];
-      lazy[RT] += lazy[x];
+    int n;
+    const int neutral = 0;           // TODO: change if needed
+    vector<int> seg, lazy;
+    vector<bool> hasLazy;
+    
+    SegTree(int sz) {
+        n = sz;
+        seg.assign(4 * n, 0);
+        lazy.assign(4 * n, 0);
+        hasLazy.assign(4 * n, false);
     }
-    seg[x] += lazy[x]; //// add lazy to the node
-    lazy[x] = 0; //// set lazy value
-  }
 
-  void update(int l, int r, int v, int x = 0, int lx = 0, int rx = -1) {
-    if (rx == -1)rx = sz - 1;
-    propagate(x, lx, rx);
-    if (l <= lx and rx <= r) {
-      lazy[x] += v; //// add lazy to the node
-      propagate(x, lx, rx);
-      return;
+private:
+    int merge(int a, int b) {
+        return a + b;                // TODO: change
     }
-    if (r < lx or rx < l)return;
-    update(l, r, v, LF, lx, md);
-    update(l, r, v, RT, md + 1, rx);
-    seg[x] = merge(seg[LF], seg[RT]);
-  }
 
-  int query(int l, int r, int x = 0, int lx = 0, int rx = -1) {
-    if (rx == -1)rx = sz - 1;
-    propagate(x, lx, rx);
-    if (l <= lx and rx <= r) return seg[x];
-    if (r < lx or rx < l) return ign;
-    return merge(
-        query(l, r, LF, lx, md),
-        query(l, r, RT, md + 1, rx)
-    );
-  }
+    void apply(int x, int lx, int rx, int v) {
+        seg[x] += 1LL * (rx - lx + 1) * v;   // TODO: change
+        lazy[x] += v;                        // TODO: change
+        hasLazy[x] = true;
+    }
 
-#undef md
-#undef LF
-#undef RT
+    void push(int x, int lx, int rx) {
+        if (!hasLazy[x] || lx == rx) return;
+
+        int mid = (lx + rx) / 2;
+
+        apply(x * 2 + 1, lx, mid, lazy[x]);
+        apply(x * 2 + 2, mid + 1, rx, lazy[x]);
+
+        lazy[x] = 0;
+        hasLazy[x] = false;
+    }
+
+    void update(int l, int r, int v, int x, int lx, int rx) {
+        if (r < lx || rx < l) return;
+
+        if (l <= lx && rx <= r) {
+            apply(x, lx, rx, v);
+            return;
+        }
+
+        push(x, lx, rx);
+
+        int mid = (lx + rx) / 2;
+
+        update(l, r, v, x * 2 + 1, lx, mid);
+        update(l, r, v, x * 2 + 2, mid + 1, rx);
+
+        seg[x] = merge(seg[x * 2 + 1], seg[x * 2 + 2]);
+    }
+
+    int query(int l, int r, int x, int lx, int rx) {
+        if (r < lx || rx < l) return neutral;
+        if (l <= lx && rx <= r) return seg[x];
+
+        push(x, lx, rx);
+
+        int mid = (lx + rx) / 2;
+
+        return merge(
+            query(l, r, x * 2 + 1, lx, mid),
+            query(l, r, x * 2 + 2, mid + 1, rx)
+        );
+    }
+
+public:
+    void update(int l, int r, int v) {
+        update(l, r, v, 0, 0, n - 1);
+    }
+
+    int query(int l, int r) {
+        return query(l, r, 0, 0, n - 1);
+    }
 };
