208501e881b2c24c50d665e01b4823f8b879e484
Mohammed Abdelaleem <166875636+Mo-3leem@users.noreply.github.com>
2026-08-08 14:41:46 +0300

    Create Flatten tree.cpp

---
+++ Trees/Flatten tree.cpp
@@ -0,0 +1,15 @@
+vector<int> flat(n);
+int timer = 0;
+vector<int> in(n), out(n);
+
+function<void(int, int)> dfs = [&](int u, int p) {
+  in[u] = timer;
+  flat[timer] = u;
+  timer++;
+  for (auto v: adj[u]) {
+    if (v == p) continue;
+    dfs(v, u);
+  }
+  out[u] = timer - 1;
+};
+dfs(0, -1);
